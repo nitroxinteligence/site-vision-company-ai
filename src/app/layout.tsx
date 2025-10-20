bfc19ba2f80ad57596daec4be8ddf4ae0b705837
Mateus M <nitroxinteligence@gmail.com>
2025-10-20 16:05:59 -0300

    fix: Corrige estrutura do layout.tsx e ajusta estratégia do GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,6 +37,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <html lang="pt-BR">
+      <body
+        className={`${inter.variable} font-sans antialiased`}
+      >
+        {/* Google Tag Manager (noscript) */}
         <noscript
           dangerouslySetInnerHTML={{
             __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5DC5JW25"
@@ -44,15 +49,13 @@ export default function RootLayout({
           }}
         />
         {/* End Google Tag Manager (noscript) */}
-        {/* Google Tag Manager */}
-        <Script id="gtm-script" strategy="afterInteractive">
+        <Script id="gtm-script" strategy="beforeInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-5DC5JW25');`}
         </Script>
-        {/* End Google Tag Manager */}
         <SmoothScrollProvider>
           <ConditionalNavbar />
           {children}
