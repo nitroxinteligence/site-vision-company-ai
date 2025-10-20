20f05682a05ac577d945392c8f6353eddff5ccbe
Mateus M <nitroxinteligence@gmail.com>
2025-10-20 16:01:36 -0300

    feat: Adiciona Google Tag Manager ao site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { SmoothScrollProvider } from "@/components/providers/smooth-scroll-provider";
 import { ConditionalNavbar } from "@/components/ui/conditional-navbar";
@@ -37,9 +38,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      {/* Google Tag Manager */}
+      <Script id="gtm-script" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5DC5JW25');`}
+      </Script>
+      {/* End Google Tag Manager */}
       <body
         className={`${inter.variable} font-sans antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5DC5JW25"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+        {/* End Google Tag Manager (noscript) */}
         <SmoothScrollProvider>
           <ConditionalNavbar />
           {children}
