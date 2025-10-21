bf9033f73aaae77ec94e9ab4159ddbccfbf6429a
Mateus M <nitroxinteligence@gmail.com>
2025-10-21 11:09:22 -0300

    refactor: implement GTM through client component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
+import GTMProvider from "@/components/providers/GTMProvider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 import { SmoothScrollProvider } from "@/components/providers/smooth-scroll-provider";
 import { ConditionalNavbar } from "@/components/ui/conditional-navbar";
@@ -38,26 +38,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <head>
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5DC5JW25');`}
-        </Script>
-      </head>
       <body
         className={`${inter.variable} font-sans antialiased`}
       >
-        {/* Google Tag Manager (noscript) */}
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5DC5JW25"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-          }}
-        />
-        {/* End Google Tag Manager (noscript) */}
+        <GTMProvider />
         <SmoothScrollProvider>
           <ConditionalNavbar />
           {children}
