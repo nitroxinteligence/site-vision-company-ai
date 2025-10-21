de30f9059a344b0a7b28f58a055a4361bdba3fb8
Mateus M <nitroxinteligence@gmail.com>
2025-10-21 10:54:01 -0300

    fix: move GTM script to head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5DC5JW25');`}
+        </Script>
+      </head>
       <body
         className={`${inter.variable} font-sans antialiased`}
       >
@@ -49,13 +58,6 @@ export default function RootLayout({
           }}
         />
         {/* End Google Tag Manager (noscript) */}
-        <Script id="gtm-script" strategy="beforeInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5DC5JW25');`}
-        </Script>
         <SmoothScrollProvider>
           <ConditionalNavbar />
           {children}
