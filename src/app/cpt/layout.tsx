cff99ae999c365909ef34d1b5c29b51f53f73867
Mateus M <nitroxinteligence@gmail.com>
2025-10-27 13:42:43 -0300

    fix: Remove duplicação do GTM na página /cpt
    - Remove implementação duplicada do Google Tag Manager do layout específico da página /cpt
    - Mantém apenas a implementação global do GTM no layout principal
    - Corrige problema onde GTM parava de rastrear eventos ao interagir com campos do formulário
    - Elimina conflito no dataLayer causado por múltiplas instâncias do GTM

--- src/app/cpt/layout.tsx
+++ src/app/cpt/layout.tsx
@@ -1,8 +1,4 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import Script from "next/script";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "CPT - Vision AI",
@@ -14,39 +10,5 @@ export default function CptLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="pt-BR">
-      <head>
-        {/* Google Tag Manager - CPT Specific */}
-        <Script
-          id="gtm-cpt"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-5DC5JW25');
-            `,
-          }}
-        />
-        {/* End Google Tag Manager */}
-      </head>
-      <body className={inter.className}>
-        {/* Google Tag Manager (noscript) - CPT Specific */}
-        <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5DC5JW25"
-            height="0" 
-            width="0" 
-            style={{display: 'none', visibility: 'hidden'}}
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
-        
-        {children}
-      </body>
-    </html>
-  );
+  return children;
 }
